a76ed745681703f6f5912366671dda904b519a87
fajarriv <rivaldiibnusina@gmail.com>
2023-04-16 21:29:09 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/rentingandbooking/RentingAndBookingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.rentingandbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentingAndBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentingAndBookingApplication.class, args);
+    }
+
+}
