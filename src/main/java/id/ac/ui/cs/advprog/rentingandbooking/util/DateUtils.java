d50267ffda1c570c5d002f0a590d042832f6eb8d
fajarriv <rivaldiibnusina@gmail.com>
2023-05-18 00:13:24 +0700

    refactor dateUtils

--- src/main/java/id/ac/ui/cs/advprog/rentingandbooking/util/DateUtils.java
+++ src/main/java/id/ac/ui/cs/advprog/rentingandbooking/util/DateUtils.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.rentingandbooking.util;
 
+import lombok.experimental.UtilityClass;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@UtilityClass
 public class DateUtils {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd"; // The expected format of the date string in JSON
