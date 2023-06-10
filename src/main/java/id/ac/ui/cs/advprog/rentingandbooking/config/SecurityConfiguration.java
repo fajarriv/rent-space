9e44fcb7f1537aceea8442a6efb08a7db8d03ec1
fajarriv <rivaldiibnusina@gmail.com>
2023-06-10 19:35:09 +0700

    change requestmatchers

--- src/main/java/id/ac/ui/cs/advprog/rentingandbooking/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/rentingandbooking/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("*")
+                .requestMatchers("/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
